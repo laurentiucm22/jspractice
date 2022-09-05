ccb1ba006ea1372a97a63efb3836f2f09d6326c9
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-09-05 15:16:45 +0300

    Finished part 1 js fundamentals

--- js-practice/JsFundamentals 1/app.js
+++ js-practice/JsFundamentals 1/app.js
@@ -1,76 +1,88 @@
-// prettier-ignore
-import { calculateBMI, displayBMIComparation, calculateAvarageScore } from "./functions.js";
-// prettier-ignore
-import {markMass, markHeight, jhonMass, jhonHeight, massMark, heightMark, massJhon, heightJhon, names} from "./constants.js";
+// // prettier-ignore
+// import { calculateBMI, displayBMIComparation, calculateAvarageScore } from "./functions.js";
+// // prettier-ignore
+// import {markMass, markHeight, jhonMass, jhonHeight, massMark, heightMark, massJhon, heightJhon, names} from "./constants.js";
 
-// Operations
-// Calculate BMI first course challenge
-// Case 1
-const markBMI = calculateBMI(markMass, markHeight);
-const jhonBMI = calculateBMI(jhonMass, jhonHeight);
-// Case 2
-const bmiMark = calculateBMI(massMark, heightMark);
-const bmiJhon = calculateBMI(massJhon, heightJhon);
-// Second chanllenge
-const result = displayBMIComparation(markBMI, jhonBMI, names);
-const secondResult = displayBMIComparation(bmiMark, bmiJhon, names);
+// // Operations
+// // Calculate BMI first course challenge
+// // Case 1
+// const markBMI = calculateBMI(markMass, markHeight);
+// const jhonBMI = calculateBMI(jhonMass, jhonHeight);
+// // Case 2
+// const bmiMark = calculateBMI(massMark, heightMark);
+// const bmiJhon = calculateBMI(massJhon, heightJhon);
+// // Second chanllenge
+// const result = displayBMIComparation(markBMI, jhonBMI, names);
+// const secondResult = displayBMIComparation(bmiMark, bmiJhon, names);
 
 // =================================================================
 // Coding Challenge #3
 // Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
 
 // Team 1 scores
-let dolfRounds = [96, 108, 89];
-// Team 2 scores
-let koalRounds = [88, 91, 110];
+// let dolfRounds = [96, 108, 89];
+// // Team 2 scores
+// let koalRounds = [88, 91, 110];
 
-let resultTeamA = calculateAvarageScore(dolfRounds);
-let resultTeamB = calculateAvarageScore(koalRounds);
-console.log(resultTeamA.toFixed(1), resultTeamB.toFixed(1));
+// let resultTeamA = calculateAvarageScore(dolfRounds);
+// let resultTeamB = calculateAvarageScore(koalRounds);
+// console.log(resultTeamA.toFixed(1), resultTeamB.toFixed(1));
 
-if (resultTeamA > resultTeamB && resultTeamA >= 100) {
-  console.log("Team Dolphins is the winner! 🏆");
-} else if (resultTeamA < resultTeamB && resultTeamB >= 100) {
-  console.log("Team Koalas is the winner! 🏆");
-} else if (resultTeamA === resultTeamB && resultTeamA && resultTeamB >= 100) {
-  console.log("Both team win! 🎆");
-} else {
-  console.log("No one wins! 😭");
-}
+// if (resultTeamA > resultTeamB && resultTeamA >= 100) {
+//   console.log("Team Dolphins is the winner! 🏆");
+// } else if (resultTeamA < resultTeamB && resultTeamB >= 100) {
+//   console.log("Team Koalas is the winner! 🏆");
+// } else if (resultTeamA === resultTeamB && resultTeamA && resultTeamB >= 100) {
+//   console.log("Both team win! 🎆");
+// } else {
+//   console.log("No one wins! 😭");
+// }
 
-// Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
-dolfRounds = [97, 112, 123];
-koalRounds = [109, 95, 101];
+// // Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// dolfRounds = [97, 112, 123];
+// koalRounds = [109, 95, 101];
 
-resultTeamA = calculateAvarageScore(dolfRounds);
-resultTeamB = calculateAvarageScore(koalRounds);
-console.log(resultTeamA.toFixed(1), resultTeamB.toFixed(1));
+// resultTeamA = calculateAvarageScore(dolfRounds);
+// resultTeamB = calculateAvarageScore(koalRounds);
+// console.log(resultTeamA.toFixed(1), resultTeamB.toFixed(1));
 
-if (resultTeamA > resultTeamB && resultTeamA >= 100) {
-  console.log("Team Dolphins is the winner! 🏆");
-} else if (resultTeamA < resultTeamB && resultTeamB >= 100) {
-  console.log("Team Koalas is the winner! 🏆");
-} else if (resultTeamA === resultTeamB && resultTeamA && resultTeamB >= 100) {
-  console.log("Both team win! 🎆");
-} else {
-  console.log("No one wins! 😭");
-}
+// if (resultTeamA > resultTeamB && resultTeamA >= 100) {
+//   console.log("Team Dolphins is the winner! 🏆");
+// } else if (resultTeamA < resultTeamB && resultTeamB >= 100) {
+//   console.log("Team Koalas is the winner! 🏆");
+// } else if (resultTeamA === resultTeamB && resultTeamA && resultTeamB >= 100) {
+//   console.log("Both team win! 🎆");
+// } else {
+//   console.log("No one wins! 😭");
+// }
 
-// Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+// // Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 
-dolfRounds = [97, 112, 101];
-koalRounds = [109, 95, 106];
+// dolfRounds = [97, 112, 101];
+// koalRounds = [109, 95, 106];
 
-resultTeamA = calculateAvarageScore(dolfRounds);
-resultTeamB = calculateAvarageScore(koalRounds);
-console.log(resultTeamA.toFixed(1), resultTeamB.toFixed(1));
+// resultTeamA = calculateAvarageScore(dolfRounds);
+// resultTeamB = calculateAvarageScore(koalRounds);
+// console.log(resultTeamA.toFixed(1), resultTeamB.toFixed(1));
 
-if (resultTeamA > resultTeamB && resultTeamA >= 100) {
-  console.log("Team Dolphins is the winner! 🏆");
-} else if (resultTeamA < resultTeamB && resultTeamB >= 100) {
-  console.log("Team Koalas is the winner! 🏆");
-} else if (resultTeamA === resultTeamB && resultTeamA && resultTeamB >= 100) {
-  console.log("Both team win! 🎆");
-} else {
-  console.log("No one wins! 😭");
-}
+// if (resultTeamA > resultTeamB && resultTeamA >= 100) {
+//   console.log("Team Dolphins is the winner! 🏆");
+// } else if (resultTeamA < resultTeamB && resultTeamB >= 100) {
+//   console.log("Team Koalas is the winner! 🏆");
+// } else if (resultTeamA === resultTeamB && resultTeamA && resultTeamB >= 100) {
+//   console.log("Both team win! 🎆");
+// } else {
+//   console.log("No one wins! 😭");
+// }
+
+// ==============================================
+// The Conditional (Ternary) Operator
+// Coding Challenge #4
+
+let bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `“The bill was ${bill}, the tip was ${tip}, and the total value ${
+    bill + tip
+  }”`
+);
--- js-practice/JsFundamentals 1/assignments.js
+++ js-practice/JsFundamentals 1/assignments.js
@@ -1,34 +1,34 @@
-import { takeDecisions, perfectCountry } from "./functions.js";
+// import { takeDecisions, perfectCountry } from "./functions.js";
 // ==============================================
 // Variables and Values
-const country = "England";
-const secondCountry = "Portugal";
-const continent = "Europe";
-let population = 68;
-let secondPopulation = 11;
-let avaragePopulation = 33;
-let populationInFinland = 6;
+// const country = "England";
+// const secondCountry = "Portugal";
+// const continent = "Europe";
+// let population = 68;
+// let secondPopulation = 11;
+// let avaragePopulation = 33;
+// let populationInFinland = 6;
 
 // ==============================================
 // Data Types
-const isIsland = true;
-let language = "english";
-let secondLanguage = "portuguese";
+// const isIsland = true;
+// let language = "english";
+// let secondLanguage = "portuguese";
 
 // ==============================================
 // Basic Operators
-const splitPopulationInHalf = population / 2;
+// const splitPopulationInHalf = population / 2;
 // let incresePopulationByOne = population++;
 
-let comparePopulation = populationInFinland > population;
-comparePopulation = population > avaragePopulation;
+// let comparePopulation = populationInFinland > population;
+// comparePopulation = population > avaragePopulation;
 
 // ==============================================
 // Strings and Template Literals
 // prettier-ignore
-let description = "Portugal is in Europe, and its 11 million people speak portuguese";
+// let description = "Portugal is in Europe, and its 11 million people speak portuguese";
 
-description = `${secondCountry} is in ${continent}, and it's ${secondPopulation} million people speak ${secondLanguage}`;
+// description = `${secondCountry} is in ${continent}, and it's ${secondPopulation} million people speak ${secondLanguage}`;
 
 // ==============================================
 // if / else Statements
@@ -37,15 +37,15 @@ description = `${secondCountry} is in ${continent}, and it's ${secondPopulation}
 // ==============================================
 // Type Conversion and Coercion
 // Predict Results
-let a = "9" - "5"; // 4 - number
-let b = "19" - "13" + "17"; // 6 + "17" = "617"; - string
-let c = "19" - "13" + 17; // 6 + 17 = 23; - number
-let d = "123" < 57; // false - boolen
-let e = 5 + 6 + "4" + 9 - 4 - 2; // 1143 - number
+// let a = "9" - "5"; // 4 - number
+// let b = "19" - "13" + "17"; // 6 + "17" = "617"; - string
+// let c = "19" - "13" + 17; // 6 + 17 = 23; - number
+// let d = "123" < 57; // false - boolen
+// let e = 5 + 6 + "4" + 9 - 4 - 2; // 1143 - number
 // let's see if I am right
 // console.log(a, b, c, d, e);
 
-let inputNumber = "1991";
+// let inputNumber = "1991";
 // console.log(inputNumber, Number(inputNumber), +inputNumber);
 
 // ==============================================
@@ -65,3 +65,93 @@ let inputNumber = "1991";
 // Logical Operations
 // prettier-ignore
 // perfectCountry(country, language, population, isIsland);
+
+// let language = "romanian";
+
+// switch (language) {
+//   case "mandarin":
+//   case "chinese":
+//     console.log("MOST number of native speakers");
+//     break;
+//   case "spanish":
+//     console.log("2nd place in number of native speakers");
+//     break;
+//   case "english":
+//     console.log("3rd place");
+//     break;
+//   case "hindi":
+//     console.log("Number 4");
+//     break;
+//   case "arabic":
+//     console.log("5th most spoken language");
+//     break;
+//   default:
+//     console.log("Great language too 😁");
+// }
+
+// if (language === "chinese" || language === "mandarin") {
+//   console.log("MOST number of native speakers2");
+// } else if (language === "english") {
+//   console.log("3rd place");
+// } else if (language === "spanish") {
+//   console.log("2nd place in number of native speakers");
+// } else {
+//   console.log("Great language too 😁");
+// }
+
+// ==============================================
+// Switch Stament
+
+// const day = "money";
+
+// switch (day) {
+//   case "monday":
+//     console.log("Js practice");
+//     console.log("Eat");
+//     console.log("Chill and learn some piano");
+//     break;
+//   case "tuesday":
+//     console.log("Js practice");
+//     console.log("Wash the car");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Js practice");
+//     break;
+//   case "friday":
+//     console.log("Js practice");
+//     console.log("Grab a drink 🍸");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Enjoy the free time! 🧭");
+//     break;
+//   default:
+//     console.log("Not a valid day");
+// }
+
+// if (day === "monday") {
+//   console.log("Js practice");
+// } else if (day === "tuesday") {
+//   console.log("Js practice and wash car");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("Js practice and run 4 laps!");
+// } else if (day === "friday") {
+//   console.log("Js practice and piano lessons");
+// } else if (day === "saturday" || day === "sunday") {
+//   console.log("Enjoy the weekend and piano practice");
+// } else {
+//   console.log("Not a valid day!");
+// }
+
+// ==============================================
+// The Conditional (Ternary) Operator
+
+// let myCountryPopulation = 20;
+// let countryName = "Romania";
+
+// console.log(
+//   myCountryPopulation >= 33
+//     ? `${countryName}'s population is above avarage!🚩`
+//     : `${countryName}'s population is below avarage 🏴‍☠️`
+// );
